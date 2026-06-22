87155969cf8c07d262b7cd1ee3f2f6910fcfabce
haroldjunior501-cell <haroldjunior501@gmail.com>
2026-06-23 00:07:11 +0200

    Fix async_generator: use AsyncGenerator type hint

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for async_generator coroutine."""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yield 10 random floats between 0 and 10, one per second."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
